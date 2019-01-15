a41c3a65de23e2d757c213784ceb8dbe542365ed
Instabot <instabot@example.com>
2019-01-15 13:22:20 +0000

    added a webhook notification type

--- src/notifications/all.js
+++ src/notifications/all.js
@@ -1,17 +1,12 @@
 const SmsNotifier = require('./sms');
 const SlackNotifier = require('./slack');
 const TelegramNotifier = require('./telegram');
-const Webhook = require('./webhook');
 
 // A list of all the supported exchanges
 module.exports = [
     {
         name: 'sms',
         driver: new SmsNotifier(),
-    },
-    {
-        name: 'webhook',
-        driver: new Webhook(),
     },
     {
         name: 'slack',
