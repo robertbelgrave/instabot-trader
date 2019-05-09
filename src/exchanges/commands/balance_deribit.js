0f8ab7d73685c85846e06cdd0fd2e114ba60d17e
Thomas VIAL <github@ifusio.com>
2019-05-09 18:07:54 +0200

    Added `currency` parameter to support ETH.

--- src/exchanges/commands/balance_deribit.js
+++ src/exchanges/commands/balance_deribit.js
@@ -7,14 +7,16 @@ const notifier = require('../../notifications/notifier');
  * Report account details (Deribit edition)
  */
 module.exports = (context) => {
-    const { ex = {} } = context;
+    const { ex = {}, symbol = '' } = context;
     logger.progress('NOTIFY ACCOUNT BALANCE');
+    const currency = ex.splitDashedSymbol(symbol).asset;
 
-    return ex.api.account().then((account) => {
-        const msg = `Deribit: Equity: ${util.roundDown(account.equity, 4)} btc, ` +
-            `available: ${util.roundDown(account.availableFunds, 4)} btc, ` +
-            `balance: ${util.roundDown(account.balance, 4)} btc, pnl: ${util.roundDown(account.PNL, 4)} btc.`;
+    return ex.api.account(currency).then((account) => {
+        const msg = `Deribit: Equity: ${util.roundDown(account.equity, 4)} ${currency}, ` +
+            `available: ${util.roundDown(account.availableFunds, 4)} ${currency}, ` +
+            `balance: ${util.roundDown(account.balance, 4)} ${currency}, pnl: ${util.roundDown(account.PNL, 4)} ${currency}.`;
         notifier.send(msg);
         logger.results(msg);
     });
 };
+
